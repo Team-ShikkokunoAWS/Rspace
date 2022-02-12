8a84d082793a90ab6bfadb87e3c58b81f85e5fe0
yk-you0922 <wkgtyou0922@gmail.com>
2022-02-12 22:30:46 +0900

    【frontend】VueRouter 認証の制御仮組み

--- project/frontend/src/router.ts
+++ project/frontend/src/router.ts
@@ -2,12 +2,36 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import LoginPage from './pages/LoginPage.vue';
 import SignUpPage from './pages/SignUpPage.vue';
+import { store } from './store';
 
 // ルーティングパスと表示内容を配列として設定
 const routes = [
-  { path:'/', name: 'HomePage', component: HomePage },
-  { path:'/login', name: 'LoginPage', component: LoginPage },
-  { path:'/signup', name: 'SignUpPage', component: SignUpPage },
+  { path:'/', name: 'HomePage', component: HomePage,
+    beforeEnter: (_to: any, _from: any, next: any) => {
+      if (store.state.user.isLogined) {
+        next();
+      } else {
+        next('/login');
+      }
+    }
+  },
+  { path:'/login', name: 'LoginPage', component: LoginPage,
+    beforeEnter:  (_to: any, _from: any, next: any) => {
+      if (store.state.user.isLogined) {
+        next('/');
+      } else {
+        next();
+      }
+    }
+  },
+  { path:'/signup', name: 'SignUpPage', component: SignUpPage,
+  beforeEnter:  (_to: any, _from: any, next: any) => {
+    if (store.state.user.isLogined) {
+      next('/');
+    } else {
+      next();
+    }
+  } },
 ];
 
 const router = createRouter({

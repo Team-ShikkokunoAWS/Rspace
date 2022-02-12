6cd154872851bf5648d1fab8e35a79052afb8845
yk-you0922 <wkgtyou0922@gmail.com>
2022-02-12 15:35:32 +0900

    【frontend】Headerコンポーネント作成・追加

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <router-view />
+  <div>
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Header from './components/Header.vue';
 
 @Options({
   components: {
+    Header
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100vh;
+  }
 </style>
---
+++ project/frontend/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header-container">
+    <h1 class="header-title">RSpace</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { 
+  defineComponent,
+} from 'vue';
+
+export default defineComponent ({
+  name: 'Header',
+  components: {
+
+  },
+  setup() {
+
+    return {
+
+    }
+  }
+}) // export default defineComponent
+</script>
+
+<style scoped>
+.header-container {
+  width: 100%;
+  height: 80px;
+  border-bottom: 1.5px solid #333;
+}
+
+.header-title {
+  margin-left: 20px;
+}
+
+</style>

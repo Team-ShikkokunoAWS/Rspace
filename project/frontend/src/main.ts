fd64b18e2a7929d969ddf3a8031b582669ce8eea
yk-you0922 <wkgtyou0922@gmail.com>
2022-02-12 22:31:10 +0900

    【frontend】VueFontAwesome設定

--- project/frontend/src/main.ts
+++ project/frontend/src/main.ts
@@ -4,9 +4,15 @@ import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { store, key } from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas);
 
 const app = createApp(App);
-app.use(router);
-app.use(VueAxios, axios);
-app.use(store, key);
+app.use(router); // ルーティング関連
+app.use(VueAxios, axios); // API関連
+app.use(store, key); // 状態管理関連
+app.component('fa-icon', FontAwesomeIcon) // FontAwesomeアイコン関連
 app.mount("#app");

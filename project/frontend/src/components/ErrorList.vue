2526bdf78130ce31c294808c03785e9411c6bdb6
Yuki Arami <wkgtyou0922@gmail.com>
2022-02-13 17:02:45 +0900

    【frontend】エラーコンポーネント：新規作成

---
+++ project/frontend/src/components/ErrorList.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<ul>
+			<li
+				class="error-message"
+				v-for="errorMessage in props.errorMessages"
+				:key="errorMessage"
+			>
+				{{ errorMessage }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+interface State {
+	errorMessage: Array<String>;
+}
+
+export default defineComponent({
+	name: 'ErrorList',
+	props: {
+		errorMessages: {
+			type: Object as PropType<State>,
+		},
+	},
+	setup(props) {
+		return {
+			props,
+		};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped>
+.error-message {
+	list-style: none;
+	color: red;
+}
+</style>

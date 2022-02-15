a66dfc862abefdbc87dd8e2b6c8f16f368576e06
Yuki Arami <wkgtyou0922@gmail.com>
2022-02-15 18:48:42 +0900

    【frontend】メッセージトースト：ゲストログイン時、メッセージトースト表示機能追加

---
+++ project/frontend/src/components/ToastMessage.vue
@@ -0,0 +1,64 @@
+<template>
+	<div v-show="toast.isShow" class="toast-container" :style="styles">
+		<div class="toast-message">
+			{{ toast.message }}
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+	name: 'ToastMessage',
+	components: {},
+	setup() {
+		// storeを取得する
+		const store = useStore();
+		const toast = computed(() => store.state.toast);
+		const styles = computed(() => {
+			return {
+				'--toast-background-color':
+					store.state.toast.toastType === 'success'
+						? '#00fa9a'
+						: store.state.toast.toastType === 'warning'
+						? '#ffd700'
+						: store.state.toast.toastType === 'danger'
+						? '#ff4500'
+						: '',
+				'--toast-message-color':
+					store.state.toast.toastType === 'success'
+						? '#eee'
+						: store.state.toast.toastType === 'warning'
+						? '#555'
+						: store.state.toast.toastType === 'danger'
+						? '#eee'
+						: '',
+			};
+		});
+		return {
+			toast,
+			styles,
+		};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped>
+.toast-container {
+	width: 100%;
+	height: 80px;
+	background-color: var(
+		--toast-background-color
+	); /** toastTypeの値によって変化させる */
+	position: absolute; /** 浮いているように見える */
+}
+.toast-message {
+	color: var(--toast-message-color); /** toastTypeの値によって変化させる */
+	font-weight: bold;
+	text-align: center;
+	line-height: 80px;
+	font-size: 25px;
+}
+</style>

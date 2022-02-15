3b68e79702d8290c226ac93d4c54019df474c947
Yuki Arami <wkgtyou0922@gmail.com>
2022-02-15 18:47:06 +0900

    【frontend】MessageManager：メッセージ追加

--- project/frontend/src/constants/MessageManager.ts
+++ project/frontend/src/constants/MessageManager.ts
@@ -2,6 +2,7 @@ export namespace MessageManager {
 	export const MSG_001 = '{0}の入力は必須です';
 	export const MSG_002 = '{0}の入力は半角英数字、記号のみです';
 	export const MSG_003 = '{0}の入力は{1}文字以内です';
+	export const MSG_004 = '{0}に成功しました';
 }
 
 export const getMessage = (message: string, param: Array<string>): string => {

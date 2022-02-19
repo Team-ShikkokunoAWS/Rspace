db7207c642c65e2464f0b142cd440393627b7950
ロロ雑巾 <konbu-dashi@Air-rorozoukin.lan>
2022-02-19 22:13:14 +0900

    finish user create function

--- project/backend/app/controllers/api/v1/users_controller.rb
+++ project/backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,46 @@
+require 'securerandom'
+require 'digest/md5'
+
 class Api::V1::UsersController < ApplicationController
+  def index
+  end
+
+  def create
+    user = user_params
+    user[:uid] = create_uid
+    user[:password] = encryption_password(user[:password])
+    user = User.new(user)
+
+    if user.save
+      render status: 200, json: {status: 'SUCCESS', data: user}
+    else
+      render status: 400, json: {status: 'ERROR', data: user.errors}
+    end
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
+  def create_uid
+    begin
+      uid = SecureRandom.hex()
+    end while User.find_by(uid: uid)
+
+    return uid
+  end
+
+  def encryption_password(password)
+    Digest::MD5.hexdigest(password)
+  end
 end
--- project/backend/config/routes.rb
+++ project/backend/config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   # resources :rooms, only: [:index, :show, :create]
 
-  # scope '/users' do
-  #   scope '/v1' do
-  #     post 'create' => 'users#create'
-  #     post 'login' => 'users#login'
-  #   end
-  # end
-
   namespace 'api' do
     namespace 'v1' do
+      # post 'login' => 'users#login'
       resources :users
     end
   end
-
 end

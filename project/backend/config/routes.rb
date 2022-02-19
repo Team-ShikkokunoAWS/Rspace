8bdfcd970cea7d00c94cd1e9b169241fec865ae3
ロロ雑巾 <konbu-dashi@Air-rorozoukin.lan>
2022-02-19 14:01:17 +0900

    backup commit

--- project/backend/config/routes.rb
+++ project/backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'rooms#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:index, :show, :create]
 end

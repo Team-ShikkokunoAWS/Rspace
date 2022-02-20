08292f71848a96320dfc2a9ab714c2dc2f812849
Yuki Arami <wkgtyou0922@gmail.com>
2022-02-20 23:31:01 +0900

    【frontend】チャット一覧ページ・ゲーム１ページ・ゲーム２ページコンポーネント：新規作成

---
+++ project/frontend/src/components/pages/Game1Page.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Game1Page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Game1Page',
+	components: {},
+	setup() {
+		return {};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped></style>
---
+++ project/frontend/src/components/pages/Game2Page.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Game2Page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Game2Page',
+	components: {},
+	setup() {
+		return {};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped></style>

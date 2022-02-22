b0d542abb5dff8a924e7b31ca6e1b426be59d40e
Yuki Arami <wkgtyou0922@gmail.com>
2022-02-22 23:36:41 +0900

    【frontend】ユーザーページ：新規作成

---
+++ project/frontend/src/components/pages/UserPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>UserPage</h2>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'UserPage',
+	components: {},
+	setup() {
+		return {};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped></style>

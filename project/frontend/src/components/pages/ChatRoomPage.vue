a9f9b479bd6348c9e0167080d3c830fd7832567a
Yuki Arami <wkgtyou0922@gmail.com>
2022-03-03 22:25:28 +0900

    【frontend】チャットルームページ：新規作成

---
+++ project/frontend/src/components/pages/ChatRoomPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>ChatRoomPage</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'ChatRoomPage',
+	components: {},
+	setup() {
+		return {};
+	},
+}); // export default defineComponent
+</script>
+
+<style scoped></style>

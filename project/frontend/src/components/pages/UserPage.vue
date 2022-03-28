fafffbf4f62a2711059d908b8c4bedac2bffd803
Yuki Arami <wkgtyou0922@gmail.com>
2022-03-29 00:13:57 +0900

    【frontend】ユーザーページ：遷移時のAPIによる存在チェックと遷移処理の実装

--- project/frontend/src/components/pages/UserPage.vue
+++ project/frontend/src/components/pages/UserPage.vue
@@ -45,6 +45,7 @@
 import { defineComponent, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from '@/store';
+import axios from '@/plugins/axios';
 import UserIcon from '@/components/parts/UserIcon.vue';
 import CButton from '@/components/parts/CButton.vue';
 import MainCard from '@/components/parts/MainCard.vue';
@@ -98,18 +99,18 @@ export default defineComponent({
 	 ====================================*/
 	beforeRouteEnter(to, from, next) {
 		// ルーティングのuidを取得
-		const params = to.params;
-		const uid = params.uid;
-		console.log(uid);
-		// 試験的にログインuidと一致しなければHomePageへリダイレクトさせるようにする
-		// if (uid === '76b535fb855ad64430e794afaa4c3272') {
-		// ゲストログインのみ遷移できる状態
-		if (uid === 'test-1234-user-5678-abcd-9012-gues-tuse') {
-			next();
-		} else {
-			next('/');
-		}
-		console.log(to, from, next);
+		const uid = to.params.uid;
+		axios
+			.post('v1/users/show', { uid: uid })
+			.then((response) => {
+				console.log(response);
+				// マイページへ遷移
+				next();
+			})
+			.catch((error) => {
+				console.log(error);
+				next('/404NotFound');
+			});
 	},
 }); // export default defineComponent
 </script>
